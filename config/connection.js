a87379a2d8807d92ef1556fee6e811f24a37b55f
granteric498 <granteric498@yahoo.com>
2018-09-11 23:56:48 -0500

    added a lot of content

---
+++ config/connection.js
@@ -0,0 +1,19 @@
+const mysql = require("mysql");
+
+const connection = mysql.createConnection({
+    host: "localhost",
+    port: 3306,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: "burgers_db"
+});
+
+connection.connect(function (err) {
+    if (err) {
+        console.error("error connecting: " + err.stack);
+        return;
+    }
+    console.log("connected as id " + connection.threadId);
+});
+
+module.exports = connection;
