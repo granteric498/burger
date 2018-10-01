f5ba9c82f0cd33c2a8bce6ac568e6b656bb97fde
granteric498 <granteric498@yahoo.com>
2018-10-01 04:54:50 -0500

    Made small changes.

--- config/connection.js
+++ config/connection.js
@@ -2,8 +2,8 @@ var mysql = require("mysql");
 
 var connection;
 
-if (process.env.JAWSDB_URL) {
-    connection = mysql.createConnection(process.env.JAWSDB_URL);
+if (JAWSDB_URL) {
+    connection = mysql.createConnection(JAWSDB_URL);
 } else {
     connection = mysql.createConnection({
         host: "localhost",
@@ -14,5 +14,4 @@ if (process.env.JAWSDB_URL) {
 };
 
 connection.connect();
-
 module.exports = connection;
