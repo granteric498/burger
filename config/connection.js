0da849ea6c8ea67b25c756a3d8608a11d5a6af17
granteric498 <granteric498@yahoo.com>
2018-10-30 23:30:45 -0500

    Trying to fix bug.

--- config/connection.js
+++ config/connection.js
@@ -8,7 +8,7 @@ if (process.env.JAWSDB_URL) {
     connection = mysql.createConnection({
         host: "localhost",
         user: "root",
-        password: "1234",
+        password: "root",
         database: "burgers_db"
     });
 };
--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 
-var PORT = process.env.PORT || 8080;
+var PORT = process.env.PORT || 3000;
 
 var app = express();
 
@@ -27,3 +27,5 @@ app.use(routes);
 app.listen(PORT, function () {
   console.log(`Server listening on: http://localhost:${PORT}`);
 });
+
+module.exports = app;
